59527fd24a52ac112cc38d6b045b70e685b5f1ee
GalisGalisteo <galis.travelling@gmail.com>
2023-10-30 09:50:25 +0100

    ex 1 finished

--- ex1.js
+++ ex1.js
@@ -57,14 +57,35 @@ console.log("Puerta de embarque: ", flightReservation.gate);
 
 // 1. Muestra el número de vuelo
 
+flightReservation.flightNumber;
+console.log("🚀 ~ file: ex1.js:61 ~ flightReservation.flightNumber:", flightReservation.flightNumber)
+
 // 2. Muestra el nombre de la aerolínea
+
+flightReservation.airline;
+console.log("🚀 ~ file: ex1.js:66 ~ flightReservation.airline:", flightReservation.airline)
 
 // 3. Muestra TODOS los datos sobre la fecha de salida del vuelo
 
+flightReservation.departure;
+console.log("🚀 ~ file: ex1.js:71 ~ flightReservation.departure:", flightReservation.departure)
+
 // 4. Muestra únicamente el nombre aeropuerto de llegada
+
+flightReservation.arrival.airport
+console.log("🚀 ~ file: ex1.js:76 ~ flightReservation.arrival.airport:", flightReservation.arrival.airport)
 
 // 5. Muestra únicamente el peso del equipaje facturado
 
+flightReservation.baggage.checked.totalWeight
+console.log("🚀 ~ file: ex1.js:81 ~ flightReservation.baggage.checked.totalWeight:", flightReservation.baggage.checked.totalWeight)
+
 // 6. Muestra la SUMA del peso del equipaje facturado y el equipaje de cabina
 
+flightReservation.baggage.carryOn.totalWeight + flightReservation.baggage.checked.totalWeight
+console.log("🚀 ~ file: ex1.js:86 ~ flightReservation.baggage.carryOn.totalWeight + flightReservation.baggage.checked.totalWeight:", flightReservation.baggage.carryOn.totalWeight + flightReservation.baggage.checked.totalWeight)
+
 // 7. Usa el método includes para indicar si este vuelo dispone del servicio de Wi-Fi o no (true o false)
+
+flightReservation.inFlightServices.includes('Wi-Fi');
+console.log("🚀 ~ file: ex1.js:91 ~ flightReservation.inFlightServices.includes('Wi-Fi'):", flightReservation.inFlightServices.includes('Wi-Fi'))
